80f3fbe14c2a71935da44963b0a7f43b6a57f01b
Ankush Kapoor <work.ankushkapoor1626@gmail.com>
2025-06-12 15:09:31 +0530

    Fixed asm file for no seg fault

--- heap.asm
+++ heap.asm
@@ -2,7 +2,13 @@ bit32
 global memspace
 %define Heapsize (1024*1024*1024/4)
 
-Section .heap alloc noexec write nobits
+Section .data alloc noexec write progbits
 	memspace:
+		dd _memspace
+
+Section .heap alloc noexec write nobits
+	_memspace:
 		heapsize equ Heapsize
 		resd heapsize
+
+Section .note.GNU-stack noalloc
